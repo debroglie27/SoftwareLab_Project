227c0083a5fe7f56271249c0cf73a6e46484c989
Arijeet De <arijeetde@gmail.com>
2023-10-15 05:32:52 +0530

    changed command in LatexCompiler.py

--- scripts/LatexCompiler.py
+++ scripts/LatexCompiler.py
@@ -8,8 +8,8 @@ def latex_compiler():
 
     os.chdir('output')
     # Generate Report
-    os.system("pdflatex ../scripts/DataReport.tex")
-    os.system("pdflatex ../scripts/DataReport.tex")
+    os.system("pdflatex ../scripts/DataReport.tex > NUL 2>&1")
+    os.system("pdflatex ../scripts/DataReport.tex > NUL 2>&1")
 
 
 if __name__ == "__main__":

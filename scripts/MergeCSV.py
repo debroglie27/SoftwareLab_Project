01900174b8a58445d72a688a05fc6d15a8fdaf86
Arijeet De <arijeetde@gmail.com>
2023-10-27 11:14:59 +0530

    Documentation

--- scripts/MergeCSV.py
+++ scripts/MergeCSV.py
@@ -4,6 +4,27 @@ from scripts.Globals import CRICKET_FORMATS, check_folder
 
 
 def filter_files(filename, dd, mm, yyyy):
+    """
+    Check if a filename contains a date matching the specified day, month, and year.
+
+    Parameters:
+    - filename (str): The name of the file to be checked.
+    - dd (int): The day to be matched.
+    - mm (int): The month to be matched.
+    - yyyy (int): The year to be matched.
+
+    Returns:
+    - bool: True if the date in the filename matches the specified day, month, and year; otherwise, False.
+
+    This function is used to filter and identify files in a directory based on their filenames containing date information.
+    It extracts the date parts from the filename, compares them to the specified date, and returns True if there is a match.
+
+    Example usage:
+    filter_files('2023-01-01_data.csv', 1, 1, 2023)  # Returns True for matching date.
+    filter_files('2023-02-15_data.csv', 1, 1, 2023)  # Returns False for non-matching date.
+    filter_files('sample_file.csv', 1, 1, 2023)  # Returns False for a filename without a date.
+    """
+
     # Extract the date from the filename
     date_parts = filename.split('_')[2].split('.')[0].split('-')
     DD, MM, YYYY = map(int, date_parts)
@@ -11,7 +32,22 @@ def filter_files(filename, dd, mm, yyyy):
     return DD == dd and MM == mm and YYYY == yyyy
 
 
-def merge_csv(dd, mm, yyyy):
+def merge_csv(dd: int, mm: int, yyyy: int) -> None:
+    """
+    Merge CSV files containing cricket data for various formats and create an 'overall' CSV file.
+
+    Parameters:
+    - dd (int): Day of the date.
+    - mm (int): Month of the date.
+    - yyyy (int): Year of the date.
+
+    This function merges CSV files for each cricket format, excluding files with names not matching the specified date.
+    It creates an 'overall' CSV file for each cricket format by concatenating data from multiple player type CSV files.
+
+    Example usage:
+    merge_csv(1, 1, 2023)  # Merges CSV files for January 1, 2023, and creates 'overall' CSV files.
+    """
+
     print("Merging CSV Files...")
 
     folder_path = check_folder('csv')

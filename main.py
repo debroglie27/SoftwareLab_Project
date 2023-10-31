2dde58e08004d5c769d794dc7c3a46f55c304ce0
Arijeet De <arijeetde@gmail.com>
2023-10-31 17:20:22 +0530

    uncommented

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ def main_program():
     day, month, year = current_date()
 
     # Generate CSV Files
-    # web_scrapper(day, month, year)
+    web_scrapper(day, month, year)
 
     # Merge CSV Files with same Cricket Format
-    # merge_csv(day, month, year)
+    merge_csv(day, month, year)
 
     # Generate Plots
     data_plotter(day, month, year)
 
     # Compiling the Latex File
-    # latex_compiler()
+    latex_compiler()
 
 
 if __name__ == "__main__":
